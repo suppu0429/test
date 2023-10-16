7ba018a4e7f0dbcd945d671e9f0a9e1196525c81
supriya <supriyareddy0429@gmail.com>
2023-10-16 15:45:20 +0530

    init

---
+++ test2.js
@@ -0,0 +1,142 @@
+// // let x =10
+// // let x =20
+// // let x =30
+// // let x =40
+// // let x =50
+// // 10 should be false
+// // data types examples
+// // let,var, const
+// // tenary operator
+
+// let x = 400, y = 400, z = 50, a = 10, b = 1000
+// if (x < y) {
+//     console.log("y is big")
+// }
+// else if(x > y) {
+//     console.log("x is big")
+// }
+// else if(z > x) {
+//     console.log("z is big")
+// }
+// else if(a < z) {
+//     console.log("a is big")
+// }
+// else if (b > z) {
+//     console.log("b is big")
+// }
+
+// let c = 10;
+// if(c!==10) {
+// console.log("satisfied")
+// }
+// else{
+//     console.log("not satisfied")
+// }
+// let e = 30;
+
+// const isTrue = e !==20 ? true : false;
+// console.log(isTrue)
+
+
+
+/**
+ * Take two variables x and y.
+ * print x is big
+ * print y is big
+ * print x and y are equal
+ */
+// let x = 30, y = 30 
+// if(x>y) {
+//     console.log("x is big")
+// }
+// else if(x<y){
+//     console.log("y is big")
+// }
+// else{
+//     console.log("both are equal")
+// }
+
+// /**
+//  * Take a variable and value is 20
+//  * if variable is not equal to 20 print satisfied
+//  * if not pront not satisfied
+//  */
+// let a = 20  
+// if(a!==20) {
+//     console.log("satisfied")
+// }
+// else{
+//     console.log("not satisfied")
+// }
+
+// /**
+//   * Take a variable and value is 20
+//  * if variable is not equal to 20 print satisfied
+//  * if not pront not satisfied
+//  * use ternary operator
+//  */
+// let b = 20
+// const c = b!==20 ? true : false;
+// console.log(c)
+
+// let a= 30, b= 50
+// if(a === 30 || b === 20) {
+//     console.log("yes both conditions are staisfied")
+// }else {
+//     console.log("some thing went wrong")
+// }
+
+// const arr = ["1", "2", "3", "4", "5"]
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// arr.forEach((item) => {
+//     console.log(item)
+// })
+//  let text= ""
+//  let i=0
+//  do{
+//     text +="number" +i;
+//     i++ 
+//  }
+// while(i<=15)
+// console.log(text)
+
+// let a = "mahi", b = 60
+// if(a==="mahi" && b===70) {
+//     console.log("both conditons are satisfied")
+// } else{
+//     console.log("not satisfied")
+// }
+
+// const a= {
+//     firstname:"power",
+//     lastname:"shell",
+//     age:30,
+// }
+// a.location = "visual studio code"   
+//     console.log(a)
+//     console.log(a.firstname,a.lastname)
+
+// let number =[1,2,3,4,5,6]
+// let nu= number.push(7,8)
+//     console.log(number)
+//     console.log(nu)
+// let num =["suppu","mahi","jayanth"];
+// let morenum =["sudhakar","sunitha"];
+// num.push(...morenum);
+//     console.log(num)
+//     console.log(num.length)
+
+// let tech1 =[1,2,3,4,5,6];
+// let tech2 =[7,8,9,10,11,12];
+// let tech3 =tech1.concat(tech2);
+//     console.log(tech3);
+//     console.log(tech3.length)
+
+//     let reach1 =[4,7,2,92,3]
+//     reach1.pop()
+//     console.log(reach1)
+
---
+++ test3.js
@@ -0,0 +1,16 @@
+function active() {
+window.location.href="about.html"
+}
+
+function valu(){
+    let b = document.getElementById("pig").value;
+    console.log(b)
+    document.getElementById("se").innerHTML = b
+}
+let registrationForm={}
+function handleChange(e){
+//   console.log(e.target.value)
+  registrationForm[e.target.name] = e.target.value
+  console.log(registrationForm)
+}
+
